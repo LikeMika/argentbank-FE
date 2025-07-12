ec86a97999ab26ecd8c9a1fa500f34d06360f073
Michael Bricout <bricout.michael@gmail.com>
2025-07-12 04:52:27 +0200

    Adding Redux + store and Auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
 import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,17 +1,52 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { loginSuccess } from '../redux/authSlice'
+
 function SignIn() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: username, password }),
+      })
+
+      const data = await response.json()
+
+      if (response.ok) {
+        dispatch(loginSuccess(data.body.token))
+        navigate('/profile')
+      } else {
+        alert('Login failed: ' + data.message)
+      }
+    } catch (err) {
+      alert('Error: ' + err.message)
+    }
+  }
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
